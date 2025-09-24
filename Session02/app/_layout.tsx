aaaf1da43db798cb7c13fa8725442c780f100615
theminhdayma <147359915+theminhdayma@users.noreply.github.com>
2025-09-24 10:43:26 +0700

    Add Session02

---
+++ Session02/app/_layout.tsx
@@ -0,0 +1,111 @@
+import { Tabs } from "expo-router";
+import React from "react";
+
+import { HapticTab } from "@/components/haptic-tab";
+import { IconSymbol } from "@/components/ui/icon-symbol";
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+      }}
+    >
+      <Tabs.Screen
+        name="Bai1"
+        options={{
+          title: "Bai1",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Bai2"
+        options={{
+          title: "Bai2",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />{" "}
+      <Tabs.Screen
+        name="Bai3"
+        options={{
+          title: "Bai3",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />{" "}
+      <Tabs.Screen
+        name="Bai4"
+        options={{
+          title: "Bai4",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />{" "}
+      <Tabs.Screen
+        name="Bai5"
+        options={{
+          title: "Bai5",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />{" "}
+      <Tabs.Screen
+        name="Bai6"
+        options={{
+          title: "Bai6",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />{" "}
+      <Tabs.Screen
+        name="Bai7"
+        options={{
+          title: "Bai7",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />{" "}
+      <Tabs.Screen
+        name="Bai8"
+        options={{
+          title: "Bai8",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />{" "}
+      <Tabs.Screen
+        name="Bai9"
+        options={{
+          title: "Bai9",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />{" "}
+      <Tabs.Screen
+        name="Bai10"
+        options={{
+          title: "Bai10",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
